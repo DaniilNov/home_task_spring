b15a4c86d902b8484c5794fcb75638ef9a03da82
Новосельцев Даниил Владимирович <xa86@mail.ru>
2022-03-29 11:45:21 +0300

    *Убрал лишнюю реализазию интерфейса Process

--- src/main/java/com/example/home_task_spring/service/ExternalInfoProcess.java
+++ src/main/java/com/example/home_task_spring/service/ExternalInfoProcess.java
@@ -4,8 +4,10 @@ import com.example.home_task_spring.model.ExternalInfo;
 import com.example.home_task_spring.model.annotation.CheckRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class ExternalInfoProcess implements Process {
 
     @Value("${id-not-process}")
