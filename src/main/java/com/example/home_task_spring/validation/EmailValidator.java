9f2da5bcd93ad50406d2ccf8a37d5f4e4f71c0eb
Новосельцев Даниил Владимирович <xa86@mail.ru>
2022-04-19 20:43:28 +0300

    spring-boot-validation-hw *fix

--- src/main/java/com/example/home_task_spring/validation/EmailValidator.java
+++ src/main/java/com/example/home_task_spring/validation/EmailValidator.java
@@ -2,7 +2,6 @@ package com.example.home_task_spring.validation;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import java.util.Set;
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
 
@@ -11,6 +10,6 @@ public class EmailValidator implements ConstraintValidator<Email, String> {
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
-        return value.contains(AT) & value.contains(DOT);
+        return value.contains(AT) && value.contains(DOT);
     }
 }
